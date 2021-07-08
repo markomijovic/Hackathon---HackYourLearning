ca83390a689102476f1265876f4c9e864987592f
Marko Mijovic <mijovicmarko11@gmail.com>
2021-07-08 13:33:49 -0600

    marko

--- src/main/main.java
+++ src/main/main.java
@@ -3,7 +3,8 @@ package main;
 public class main {
     // marko
     //zach
-
+    // I am the captain now
+    // GEGEGEGEGEGE
     public static void main(String[] args) {
         System.out.println("x");
     }
